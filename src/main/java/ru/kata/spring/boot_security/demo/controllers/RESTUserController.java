c9cca576dc5ca8d7a10d4ec0035cdb25af57e2e4
user <irina6661234@mail.ru>
2023-12-02 16:42:01 +0300

    все работает,кроме посылки запросов на /user

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/RESTUserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/RESTUserController.java
@@ -4,23 +4,14 @@ package ru.kata.spring.boot_security.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.MyUserDetailsService;
-import ru.kata.spring.boot_security.demo.services.UserService;
-
-import javax.validation.constraints.NotNull;
-import java.security.Principal;
-import java.util.List;
 
 @RestController
 @RequestMapping("api/user")
@@ -35,15 +26,13 @@ public class RESTUserController {
 
 
     @GetMapping()
-    public ResponseEntity getUserData() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        org.springframework.security.core.userdetails.User userSecurity =
-                (org.springframework.security.core.userdetails.User) authentication.getPrincipal();
-        User user = myUserDetailsService.findByUsername(userSecurity.getUsername());
+    public ResponseEntity<User> getUserData() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = myUserDetailsService.findByUsername(userDetails.getUsername());
         if (user != null) {
-            return ResponseEntity.ok(user);
+            return new ResponseEntity<>(user, HttpStatus.OK);
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
 
